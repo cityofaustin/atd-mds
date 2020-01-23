da0c44dedb3e002e3e8d5f56f21bb04447a1d29f
Sergio Garcia <sergiogcx@gmail.com>
2020-01-23 14:09:32 -0600

    Removed GraphQL From standard library

--- mds/__init__.py
+++ mds/__init__.py
@@ -1,5 +1,4 @@
 from .MDSClient import MDSClient
 from .MDSAuth import MDSAuth
 from .MDSTimeZone import MDSTimeZone
-from .MDSGraphQLRequest import MDSGraphQLRequest
 from .MDSProvider import MDSProvider
