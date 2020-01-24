e3304bf4fca8390452b151695abad574a1f22f3d
Sergio Garcia <sergiogcx@gmail.com>
2020-01-24 14:17:49 -0600

    Created Base Client

---
+++ mds/clients/MDSClientBase.py
@@ -0,0 +1,104 @@
+import requests
+
+
+class MDSClientBase:
+
+    __slots__ = (
+        "config",
+        "params",
+        "headers",
+        "param_schema",
+        "mds_endpoint",
+        "paging",
+        "interval",
+        "timeout"
+    )
+
+    def __init__(self, config):
+        self.config = config
+        self.headers = {}
+        self.params = {}
+        self.mds_endpoint = self.config.get("mds_api_url", None)
+        self.paging = self.config.get("paging", False)
+        self.interval = self.config.get("interval", 1)
+        self.timeout = self.config.get("interval", 90)
+
+    def __paginate(self, **kwargs):
+        pass
+
+    def __request(self, **kwargs):
+        print(f"MDSClientBase::__request() Making request...")
+        data = {}
+        mds_endpoint = kwargs.get("mds_endpoint", {})
+        mds_params = kwargs.get("params", {})
+        mds_headers = kwargs.get("headers", {})
+
+        print(f"MDSClientBase::__request() Details:")
+        print(f"MDSClientBase::__request() mds_endpoint: {mds_endpoint}")
+        print(f"MDSClientBase::__request() mds_params: {mds_params}")
+        print(f"MDSClientBase::__request() mds_headers: {mds_headers}")
+
+        response = requests.get(
+            mds_endpoint,
+            params=mds_params,
+            headers=mds_headers
+        )
+
+        if response.status_code == 200:
+            data = {
+                "status_code": response.status_code,
+                # "response_headers": response.headers,
+                "type": "success",
+                "message": "success",
+                "data": response.json()
+            }
+        elif response.status_code == 404:
+            data = {
+                "status_code": response.status_code,
+                # "response_headers": response.headers,
+                "type": "error",
+                "message": "not found",
+                "data": {}
+            }
+        else:
+            data = {
+                "status_code": response.status_code,
+                # "response_headers": response.headers,
+                "type": "error",
+                "message": "unknown error",
+                "data": {}
+            }
+
+        return data
+
+    def set_header(self, key, value):
+        print(f"MDSClientBase::set_header() Set header k: '{key}', v: '{value}'")
+        self.headers[key] = value
+
+    def render_settings(self, headers):
+        # 1. Consolidate current headers and new headers
+        print("MDSClientBase::render_settings() Rendering headers")
+        self.headers = {**self.headers, **headers}
+
+        # 2. Initialize Param Schema & Overrides
+        print("MDSClientBase::render_settings() Rendering parameters")
+        params_override = self.config.get("mds_param_override", None)
+        if isinstance(params_override, dict):
+            for key, value in params_override.items():
+                self.param_schema[key] = value
+
+    def get_headers(self):
+        print("MDSClientBase::get_headers() returning headers")
+        return self.headers
+
+    def get_trips(self, start_time, end_time):
+
+        print("MDSClientBase::get_trips() Getting trips: %s %s " % (start_time, end_time))
+        self.params[self.param_schema["start_time"]] = start_time
+        self.params[self.param_schema["end_time"]] = end_time
+
+        return self.__request(
+            mds_endpoint=f"{self.mds_endpoint}/trips",
+            params=self.params,
+            headers=self.headers
+        )
