eda1350fc7cc2700975649524d99a26bf749ee19
Sergio Garcia <sergiogcx@gmail.com>
2020-02-07 16:42:55 -0600

    Refactored GraphQL request helper class

--- MDSGraphQLRequest.py
+++ MDSGraphQLRequest.py
@@ -1,40 +1,11 @@
-#
-#
-#
-
-from string import Template
+import requests
 
 
 class MDSGraphQLRequest:
+    __slots__ = ["endpoint", "http_params", "http_auth_token", "data", "response"]
 
-    model = {
-        "query_name": None,
-        "query_type": None,
-        "query_arguments": None,
-        "table_name": None,
-        "table_fields": {},
-        "table_values": {}
-    }
-
-    def get_model(self):
-        return self.model
-
-    def set_model_value(self, model_property, value):
-        self.model[model_property] = value
-
-    def get_model_value(self, model_property, default_value):
-        return self.model.get(model_property, default_value)
-
-    def generate_query(self):
-
-        template = Template("""
-        $query_type $query_name $query_arguments {
-            $table_name {
-                $table_values
-            }
-        }
-        """)
-
-        return template.substitute(query_type=self.get_model_value("query_type", "query"))
-
-
+    def __init__(self, endpoint, http_auth_token, **kwargs):
+        self.endpoint = endpoint
+        self.http_auth_token = http_auth_token
+        self.http_params = kwargs.get("http_params", None)
+        self.response = None
