3ce034dc12a733a365be31092db9536ecfabcedb
Sergio Garcia <sergiogcx@gmail.com>
2020-02-07 16:43:33 -0600

    Added helper methods to graphql requests

--- MDSGraphQLRequest.py
+++ MDSGraphQLRequest.py
@@ -9,3 +9,29 @@ class MDSGraphQLRequest:
         self.http_auth_token = http_auth_token
         self.http_params = kwargs.get("http_params", None)
         self.response = None
+
+    def show_config(self):
+        print({
+            "endpoint": self.endpoint,
+            "http_params": self.http_params,
+            "http_auth_token": self.http_auth_token
+        })
+
+    def request(self, query):
+        headers = {
+            "Accept": "application/json",
+            "x-hasura-admin-secret": f"Bearer {self.http_auth_token}"
+        }
+
+        self.response = requests.post(
+            self.endpoint,
+            params=self.http_params,
+            headers=headers,
+            data=query
+        )
+
+        self.response.encoding = "utf-8"
+        return self.response.json()
+
+    def get_last_response(self):
+        return self.response.json()
