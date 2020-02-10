fb10aca738cff9f270353e12a1ff58e2c3856760
Sergio Garcia <sergiogcx@gmail.com>
2020-02-09 19:06:48 -0600

    Added two basic tests for MDSAWS unit test

--- tests/test_mds_aws.py
+++ tests/test_mds_aws.py
@@ -18,3 +18,23 @@ class TestMDSAWS:
     @classmethod
     def teardown_class(cls):
         print("All tests finished for: TestMDSAWS")
+
+    def test_constructor(self):
+        mds_aws = MDSAWS(
+            bucket_name=mds_config.ATD_MDS_BUCKET,
+            aws_default_region=mds_config.ATD_MDS_REGION,
+            aws_access_key_id=mds_config.ATD_MDS_ACCESS_KEY,
+            aws_secret_access_key=mds_config.ATD_MDS_SECRET_ACCESS_KEY
+        )
+        assert isinstance(mds_aws, MDSAWS)
+
+    def test_get_config(self):
+        mds_aws = MDSAWS(
+            bucket_name=mds_config.ATD_MDS_BUCKET,
+            aws_default_region=mds_config.ATD_MDS_REGION,
+            aws_access_key_id=mds_config.ATD_MDS_ACCESS_KEY,
+            aws_secret_access_key=mds_config.ATD_MDS_SECRET_ACCESS_KEY
+        )
+        aws_config = mds_aws.get_config()
+        print(aws_config)
+        assert isinstance(aws_config, dict)
