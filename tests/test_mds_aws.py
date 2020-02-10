3c6ae1a7a0d705f096699da32c25eae837e313a9
Sergio Garcia <sergiogcx@gmail.com>
2020-02-09 19:20:42 -0600

    Added tests for json validator

--- tests/test_mds_aws.py
+++ tests/test_mds_aws.py
@@ -38,3 +38,88 @@ class TestMDSAWS:
         aws_config = mds_aws.get_config()
         print(aws_config)
         assert isinstance(aws_config, dict)
+
+    def test_load_valid_json_t1(self):
+        json_document = """
+        {
+            "glossary": {
+                "title": "example glossary",
+                "GlossDiv": {
+                    "title": "S",
+                    "GlossList": {
+                        "GlossEntry": {
+                            "ID": "SGML",
+                            "SortAs": "SGML",
+                            "GlossTerm": "Standard Generalized Markup Language",
+                            "Acronym": "SGML",
+                            "Abbrev": "ISO 8879:1986",
+                            "GlossDef": {
+                                "para": "A meta-markup language, used to create markup languages such as DocBook.",
+                                "GlossSeeAlso": ["GML", "XML"]
+                            },
+                            "GlossSee": "markup"
+                        }
+                    }
+                }
+            }
+        }
+        """
+        mds_aws = MDSAWS(
+            bucket_name=mds_config.ATD_MDS_BUCKET,
+            aws_default_region=mds_config.ATD_MDS_REGION,
+            aws_access_key_id=mds_config.ATD_MDS_ACCESS_KEY,
+            aws_secret_access_key=mds_config.ATD_MDS_SECRET_ACCESS_KEY
+        )
+
+        mds_aws.set_json_document(json_document=json_document)
+        assert isinstance(mds_aws.json_document, str)
+
+    def test_load_valid_json_t2(self):
+        json_document = "{}"
+        mds_aws = MDSAWS(
+            bucket_name=mds_config.ATD_MDS_BUCKET,
+            aws_default_region=mds_config.ATD_MDS_REGION,
+            aws_access_key_id=mds_config.ATD_MDS_ACCESS_KEY,
+            aws_secret_access_key=mds_config.ATD_MDS_SECRET_ACCESS_KEY
+        )
+        mds_aws.set_json_document(json_document=json_document)
+        assert isinstance(mds_aws.json_document, str)
+
+    def test_load_invalid_json_t1(self):
+        json_document = """
+            {
+                "This is not a valid json object"
+            }
+        """
+        mds_aws = MDSAWS(
+            bucket_name=mds_config.ATD_MDS_BUCKET,
+            aws_default_region=mds_config.ATD_MDS_REGION,
+            aws_access_key_id=mds_config.ATD_MDS_ACCESS_KEY,
+            aws_secret_access_key=mds_config.ATD_MDS_SECRET_ACCESS_KEY
+        )
+        mds_aws.set_json_document(json_document=json_document)
+        assert isinstance(mds_aws.json_document, str) is False
+
+    def test_load_invalid_json_t2(self):
+        mds_aws = MDSAWS(
+            bucket_name=mds_config.ATD_MDS_BUCKET,
+            aws_default_region=mds_config.ATD_MDS_REGION,
+            aws_access_key_id=mds_config.ATD_MDS_ACCESS_KEY,
+            aws_secret_access_key=mds_config.ATD_MDS_SECRET_ACCESS_KEY
+        )
+        mds_aws.set_json_document(json_document=None)
+        assert isinstance(mds_aws.json_document, str) is False
+
+    def test_load_invalid_json_t3(self):
+        json_document = {
+            "valid": "dictionary",
+            "but": "not string"
+        }
+        mds_aws = MDSAWS(
+            bucket_name=mds_config.ATD_MDS_BUCKET,
+            aws_default_region=mds_config.ATD_MDS_REGION,
+            aws_access_key_id=mds_config.ATD_MDS_ACCESS_KEY,
+            aws_secret_access_key=mds_config.ATD_MDS_SECRET_ACCESS_KEY
+        )
+        mds_aws.set_json_document(json_document=json_document)
+        assert isinstance(mds_aws.json_document, str) is False
