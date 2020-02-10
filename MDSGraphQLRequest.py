ffcf4078ae9b2c86b6ebd26a80b6b1b1495c5f6f
Sergio Garcia <sergiogcx@gmail.com>
2020-02-10 15:28:14 -0600

    Fixed graphql query with Hasura http header

--- MDSGraphQLRequest.py
+++ MDSGraphQLRequest.py
@@ -1,16 +1,18 @@
 import requests
-
+import logging
 
 class MDSGraphQLRequest:
     __slots__ = ["endpoint", "http_params", "http_auth_token", "data", "response"]
 
     def __init__(self, endpoint, http_auth_token, **kwargs):
+        logging.debug("MDSGraphQLRequest::__init__() Initializing HTTP GraphQL Request")
         self.endpoint = endpoint
         self.http_auth_token = http_auth_token
         self.http_params = kwargs.get("http_params", None)
         self.response = None
 
     def get_config(self):
+        logging.debug("MDSGraphQLRequest::get_config() getting config")
         return {
             "endpoint": self.endpoint,
             "http_params": self.http_params,
@@ -18,16 +20,22 @@ class MDSGraphQLRequest:
         }
 
     def request(self, query):
+        logging.debug("MDSGraphQLRequest::request() Making request")
         headers = {
-            "Accept": "application/json",
-            "x-hasura-admin-secret": f"Bearer {self.http_auth_token}"
+            "Accept": "*/*",
+            "content-type": "application/json",
+            "x-hasura-admin-secret": f"{self.http_auth_token}"
         }
+
+        logging.debug(f"MDSGraphQLRequest::request() Headers: {str(headers)}")
 
         self.response = requests.post(
             self.endpoint,
             params=self.http_params,
             headers=headers,
-            data=query
+            json={
+                "query": query
+            }
         )
 
         self.response.encoding = "utf-8"
