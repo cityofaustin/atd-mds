1402171396e224ea064395182537f72261b02d83
Sergio Garcia <sergiogcx@gmail.com>
2020-02-11 13:59:58 -0600

    Added GeoJson to Config

--- MDSConfig.py
+++ MDSConfig.py
@@ -5,6 +5,7 @@ import json
 import logging
 from MDSAWS import MDSAWS
 
+
 class MDSConfig:
     __slots__ = [
         "ATD_MDS_REGION",
@@ -15,6 +16,9 @@ class MDSConfig:
         "ATD_MDS_STAGE",
         "ATD_MDS_PROVIDERS",
         "ATD_MDS_SETTINGS",
+        "ATD_MDS_CENSUS_GEOJSON",
+        "ATD_MDS_DISTRICTS_GEOJSON",
+        "ATD_MDS_HEX_GEOJSON",
         "_MDS_SETTINGS",
         "_MDS_PROVIDERS",
         "_MDS_AWS",
@@ -28,9 +32,15 @@ class MDSConfig:
         self.ATD_MDS_BUCKET = os.getenv("ATD_MDS_BUCKET", None)
         self.ATD_MDS_STAGE = os.getenv("ATD_MDS_RUN_MODE", "STAGING")
         self.ATD_MDS_PROVIDERS = os.getenv("ATD_MDS_PROVIDERS", "config/providers.json")
-        self.ATD_MDS_SETTINGS = os.getenv("ATD_MDS_PROVIDERS", f"config/settings_{self.ATD_MDS_STAGE.lower()}.json")
-        self.ATD_MDS_CENSUS_GEOJSON = os.getenv("ATD_MDS_CENSUS_GEOJSON", "data/census_tracts_2010_simplified_20pct.json")
-        self.ATD_MDS_COUNCIL_DIST_GEOJSON = os.getenv("ATD_MDS_COUNCIL_DIST_GEOJSON", "data/council_districts_simplified.json")
+        self.ATD_MDS_SETTINGS = os.getenv(
+            "ATD_MDS_PROVIDERS", f"config/settings_{self.ATD_MDS_STAGE.lower()}.json"
+        )
+        self.ATD_MDS_CENSUS_GEOJSON = os.getenv(
+            "ATD_MDS_CENSUS_GEOJSON", "data/census_tracts_2010_simplified_20pct.json"
+        )
+        self.ATD_MDS_DISTRICTS_GEOJSON = os.getenv(
+            "ATD_MDS_DISTRICTS_GEOJSON", "data/council_districts_simplified.json"
+        )
         self.ATD_MDS_HEX_GEOJSON = os.getenv("ATD_MDS_HEX_GEOJSON", "data/hex1000.json")
         self._MDS_AWS = self._initialize_aws()
         # Internal
@@ -40,15 +50,15 @@ class MDSConfig:
     def get_config(self):
         logging.debug("MDSConfig::print() printing configuration...")
         return {
-                "ATD_MDS_REGION": self.ATD_MDS_REGION,
-                "ATD_MDS_ACCESS_KEY": self.ATD_MDS_ACCESS_KEY,
-                "ATD_MDS_SECRET_ACCESS_KEY": self.ATD_MDS_SECRET_ACCESS_KEY,
-                "ATD_MDS_BUCKET": self.ATD_MDS_BUCKET,
-                "ATD_MDS_STAGE": self.ATD_MDS_STAGE,
-                "ATD_MDS_PROVIDERS": self.ATD_MDS_PROVIDERS,
-                "ATD_MDS_SETTINGS": self.ATD_MDS_SETTINGS,
-                "_MDS_SETTINGS": self._MDS_SETTINGS,
-                "_MDS_PROVIDERS": self._MDS_PROVIDERS,
+            "ATD_MDS_REGION": self.ATD_MDS_REGION,
+            "ATD_MDS_ACCESS_KEY": self.ATD_MDS_ACCESS_KEY,
+            "ATD_MDS_SECRET_ACCESS_KEY": self.ATD_MDS_SECRET_ACCESS_KEY,
+            "ATD_MDS_BUCKET": self.ATD_MDS_BUCKET,
+            "ATD_MDS_STAGE": self.ATD_MDS_STAGE,
+            "ATD_MDS_PROVIDERS": self.ATD_MDS_PROVIDERS,
+            "ATD_MDS_SETTINGS": self.ATD_MDS_SETTINGS,
+            "_MDS_SETTINGS": self._MDS_SETTINGS,
+            "_MDS_PROVIDERS": self._MDS_PROVIDERS,
         }
 
     def _initialize_aws(self):
@@ -56,7 +66,7 @@ class MDSConfig:
             bucket_name=self.ATD_MDS_BUCKET,
             aws_default_region=self.ATD_MDS_REGION,
             aws_access_key_id=self.ATD_MDS_ACCESS_KEY,
-            aws_secret_access_key=self.ATD_MDS_SECRET_ACCESS_KEY
+            aws_secret_access_key=self.ATD_MDS_SECRET_ACCESS_KEY,
         )
 
     def _load_json_file_s3(self, key):
