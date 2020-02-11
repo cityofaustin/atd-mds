420bd489c06ea647b9c12f3f178cacbc96d22cee
Sergio Garcia <sergiogcx@gmail.com>
2020-02-11 13:49:52 -0600

    Added GeoJson config

--- MDSConfig.py
+++ MDSConfig.py
@@ -29,6 +29,9 @@ class MDSConfig:
         self.ATD_MDS_STAGE = os.getenv("ATD_MDS_RUN_MODE", "STAGING")
         self.ATD_MDS_PROVIDERS = os.getenv("ATD_MDS_PROVIDERS", "config/providers.json")
         self.ATD_MDS_SETTINGS = os.getenv("ATD_MDS_PROVIDERS", f"config/settings_{self.ATD_MDS_STAGE.lower()}.json")
+        self.ATD_MDS_CENSUS_GEOJSON = os.getenv("ATD_MDS_CENSUS_GEOJSON", "data/census_tracts_2010_simplified_20pct.json")
+        self.ATD_MDS_COUNCIL_DIST_GEOJSON = os.getenv("ATD_MDS_COUNCIL_DIST_GEOJSON", "data/council_districts_simplified.json")
+        self.ATD_MDS_HEX_GEOJSON = os.getenv("ATD_MDS_HEX_GEOJSON", "data/hex1000.json")
         self._MDS_AWS = self._initialize_aws()
         # Internal
         self._MDS_PROVIDERS = self._load_json_file_s3(key=self.ATD_MDS_PROVIDERS)
