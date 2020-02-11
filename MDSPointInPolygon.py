3b7c56bef7e01fa186a92dc3cc9cd0f3f7c05187
Sergio Garcia <sergiogcx@gmail.com>
2020-02-11 13:50:23 -0600

    Added point in polygon class placeholder

---
+++ MDSPointInPolygon.py
@@ -0,0 +1,45 @@
+import logging
+import json
+import os
+import pdb
+import time
+
+import requests
+from rtree import index
+from shapely.geometry import shape, point
+
+
+class MDSPointInPoly:
+    __slots__ = [
+        "CENSUS_TRACTS_GEOJSON",
+        "DISTRICTS_GEOJSON",
+    ]
+
+    @staticmethod
+    def read_json(file_path) -> dict:
+        """
+        Load (geo)JSON into memory
+        :param str file_path:
+        :return dict:
+        """
+        with open(file_path, "r") as fin:
+            return json.loads(fin.read())
+
+    @staticmethod
+    def create_points(data) -> dict:
+        """
+        Create shapely geometry from list of dicts
+        """
+        for row in data:
+
+            if row["x"] and row["y"]:
+                try:
+                    row["geometry"] = point.Point(float(row["x"]), float(row["y"]))
+                except:
+                    row["geometry"] = None
+            else:
+                row["geometry"] = None
+        return data
+
+    def __init__(self):
+        pass
