69240284eda10eeee7de2036b9d5cf1e70287124
Sergio Garcia <sergiogcx@gmail.com>
2020-02-13 15:30:09 -0600

    Allocate for Max-threads

--- MDSConfig.py
+++ MDSConfig.py
@@ -14,6 +14,7 @@ class MDSConfig:
         "ATD_MDS_SECRET_ACCESS_KEY",
         "ATD_MDS_BUCKET",
         "ATD_MDS_STAGE",
+        "ATD_MDS_MAX_THREADS",
         "ATD_MDS_PROVIDERS",
         "ATD_MDS_SETTINGS",
         "ATD_MDS_CENSUS_GEOJSON",
@@ -31,6 +32,7 @@ class MDSConfig:
         self.ATD_MDS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY", None)
         self.ATD_MDS_BUCKET = os.getenv("ATD_MDS_BUCKET", None)
         self.ATD_MDS_STAGE = os.getenv("ATD_MDS_RUN_MODE", "STAGING")
+        self.ATD_MDS_MAX_THREADS = os.getenv("ATD_MDS_MAX_THREADS", 10)
         self.ATD_MDS_PROVIDERS = os.getenv("ATD_MDS_PROVIDERS", "config/providers.json")
         self.ATD_MDS_SETTINGS = os.getenv(
             "ATD_MDS_PROVIDERS", f"config/settings_{self.ATD_MDS_STAGE.lower()}.json"
