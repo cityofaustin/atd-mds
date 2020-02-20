260cfddb6123d514878ba92bb24ecd827d32ab72
Sergio Garcia <sergiogcx@gmail.com>
2020-02-20 14:10:59 -0600

    Refactored Sync Socrata Placeholder

--- provider_sync_socrata.py
+++ provider_sync_socrata.py
@@ -1,52 +1,67 @@
 #!/usr/bin/env python
 
-import time
 import click
-from mds import *
-from provider_helpers import *
-
-# Debug & Logging
+import json
 import logging
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
-logger = logging.getLogger()
-logger.disabled = False
+from datetime import datetime
+
+from mds import *
+from MDSTrip import MDSTrip
+from MDSCli import MDSCli
+from MDSConfig import MDSConfig
+from MDSAWS import MDSAWS
+from MDSPointInPolygon import MDSPointInPolygon
+from MDSGraphQLRequest import MDSGraphQLRequest
+
+logging.disable(logging.DEBUG)
+
+# Let's initialize our configuration class
+mds_config = MDSConfig()
+# Then we need to initialize our AWS class with our configuration
+mds_aws = MDSAWS(
+    aws_default_region=mds_config.ATD_MDS_REGION,
+    aws_access_key_id=mds_config.ATD_MDS_ACCESS_KEY,
+    aws_secret_access_key=mds_config.ATD_MDS_SECRET_ACCESS_KEY,
+    bucket_name=mds_config.ATD_MDS_BUCKET,
+)
+# We will need the point-in-polygon class for our trips
+mds_pip = MDSPointInPolygon(
+    mds_config=mds_config
+)
+# Both the CLI and Trips classes will need an http-graphql client
+mds_gql = MDSGraphQLRequest(
+    endpoint=mds_config.get_setting("HASURA_ENDPOINT", None),
+    http_auth_token=mds_config.get_setting("HASURA_ADMIN_KEY", None)
+)
 
 
 @click.command()
 @click.option(
-    "--provider",
-    default=None,
-    prompt="Provider",
-    help="The provider's name",
+    "--provider", default=None, help="The provider's name",
 )
 @click.option(
     "--file",
     default=None,
-    prompt="Output File",
-    help="The json file you would like to validate",
+    help="Use this flag to use a specific input file.",
 )
 @click.option(
     "--interval",
     default=None,
-    prompt="Interval in hours",
     help="Relative to the maximum time for trip end, an interval window "
-         "in hours (i.e., from 11 to noon, you would need the value of '1')"
+    "in hours (i.e., from 11 to noon, you would need the value of '1')",
 )
 @click.option(
     "--time-min",
     default=None,
-    help="The minimum time where the trip ended in format: 'yyyy-mm-dd-hh'"
+    help="The minimum time where the trip ended in format: 'yyyy-mm-dd-hh'",
 )
 @click.option(
     "--time-max",
     default=None,
-    prompt="Max. End Trip Datetime",
-    help="The maximum time where the trip ended in format: 'yyyy-mm-dd-hh'"
+    help="The maximum time where the trip ended in format: 'yyyy-mm-dd-hh'",
 )
 def run(**kwargs):
-    """
-                PLACEHOLDER
-    """
+    print("Work in progress")
 
 
 if __name__ == "__main__":
