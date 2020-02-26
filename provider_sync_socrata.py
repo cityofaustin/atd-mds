1ebe20410feaaa081dc7085473c2d1f4e3f3ba75
Sergio Garcia <sergiogcx@gmail.com>
2020-02-25 21:08:50 -0600

    Implemented MDS Socrata upsert

--- provider_sync_socrata.py
+++ provider_sync_socrata.py
@@ -1,19 +1,28 @@
 #!/usr/bin/env python
+"""
+Socrata - Exporter
+Author: Austin Transportation Department, Data & Technology Services
+Description: The purpose of this script is to gather data from Hasura
+and export it to the Socrata database.
 
-import click
+The application requires the requests, sodapy and atd-mds-client libraries:
+    https://pypi.org/project/click/
+    https://pypi.org/project/requests/
+    https://pypi.org/project/sodapy/
+    https://pypi.org/project/atd-mds-client/
+"""
+
 import json
-import logging
 from datetime import datetime
 
+import click
 from mds import *
-from MDSTrip import MDSTrip
+
 from MDSCli import MDSCli
 from MDSConfig import MDSConfig
 from MDSAWS import MDSAWS
-from MDSPointInPolygon import MDSPointInPolygon
 from MDSGraphQLRequest import MDSGraphQLRequest
-
-logging.disable(logging.DEBUG)
+from MDSSocrata import MDSSocrata
 
 # Let's initialize our configuration class
 mds_config = MDSConfig()
@@ -24,10 +33,7 @@ mds_aws = MDSAWS(
     aws_secret_access_key=mds_config.ATD_MDS_SECRET_ACCESS_KEY,
     bucket_name=mds_config.ATD_MDS_BUCKET,
 )
-# We will need the point-in-polygon class for our trips
-mds_pip = MDSPointInPolygon(
-    mds_config=mds_config
-)
+
 # Both the CLI and Trips classes will need an http-graphql client
 mds_gql = MDSGraphQLRequest(
     endpoint=mds_config.get_setting("HASURA_ENDPOINT", None),
@@ -38,16 +44,6 @@ mds_gql = MDSGraphQLRequest(
 @click.command()
 @click.option(
     "--provider", default=None, help="The provider's name",
-)
-@click.option(
-    "--file",
-    default=None,
-    help="Use this flag to use a specific input file.",
-)
-@click.option(
-    "--force",
-    is_flag=True,
-    help="Forces a schedule to run by changing its status to 0 before running.",
 )
 @click.option(
     "--interval",
@@ -66,7 +62,78 @@ mds_gql = MDSGraphQLRequest(
     help="The maximum time where the trip ended in format: 'yyyy-mm-dd-hh'",
 )
 def run(**kwargs):
-    print("Work in progress")
+    """
+        Runs the program based on the above flags, the values will be passed to kwargs as a dictionary
+        :param dict kwargs: The values specified by click decorators.
+        :return:
+        """
+    mds_cli = MDSCli(
+        mds_config=mds_config,
+        mds_gql=mds_gql,
+        provider=kwargs.get("provider", None),
+        interval=kwargs.get("interval", None),
+        time_max=kwargs.get("time_max", None),
+        time_min=kwargs.get("time_min", None),
+    )
+
+    print(f"Settings: {str(mds_cli.get_config())}")
+
+    # Check the CLI settings...
+    if mds_cli.valid_settings() is False:
+        print("Invalid settings, exiting.")
+        exit(1)
+
+    print(f"Parsed Time Max: {mds_cli.parsed_date_time_max}")
+    print(f"Parsed Time Min: {mds_cli.parsed_date_time_min}")
+    print(f"Parsed Interval: {mds_cli.parsed_interval}")
+
+    # Retrieve the Schedule Class instance
+    mds_schedule = mds_cli.initialize_schedule(
+        # Default status, it does not matter
+        status_id=0,
+        # Do not check for status, this disabled by default
+        status_check=False
+    )
+    # Gather schedule items:
+    schedule = mds_schedule.get_schedule()
+    print(f"Schedule: {json.dumps(schedule)}")
+
+    # For each schedule hour block:
+    for schedule_item in schedule:
+        print(f"Running with: {json.dumps(schedule_item)}")
+
+        # Build timezone aware interval...
+        print("Building timezone aware interval ...")
+        tz_time = MDSTimeZone(
+            date_time_now=datetime(
+                schedule_item["year"],
+                schedule_item["month"],
+                schedule_item["day"],
+                schedule_item["hour"],
+            ),
+            offset=3600,  # One hour, always
+            time_zone="US/Central",  # US/Central Timezone
+        )
+
+        # Output generated time stamps on screen
+        print("Time Start (cst):\t%s" % tz_time.get_time_start(utc=False))
+        print("Time End   (cst):\t%s" % tz_time.get_time_end(utc=False))
+        print("Time Start (utc):\t%s" % tz_time.get_time_start(utc=True))
+        print("Time End   (utc):\t%s" % tz_time.get_time_end(utc=True))
+
+        mds_socrata = MDSSocrata(
+            provider_name=mds_cli.provider,
+            mds_config=mds_config,
+            mds_gql=mds_gql
+        )
+
+        trips = mds_socrata.get_data(
+            time_min=str(tz_time.get_time_start(utc=False)),
+            time_max=str(tz_time.get_time_end(utc=False))
+        )["data"]["api_trips"]
+
+        print(json.dumps(trips))
+        mds_socrata.save(data=trips)
 
 
 if __name__ == "__main__":
